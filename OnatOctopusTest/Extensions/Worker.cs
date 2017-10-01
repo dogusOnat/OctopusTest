f757bc249b43bee303e16e27aae841b5c701d33e
dogusOnat <dogus.onat.erses@gmail.com>
2017-10-01 20:11:03 +0100

    Final

--- OnatOctopusTest/Extensions/Worker.cs
+++ OnatOctopusTest/Extensions/Worker.cs
@@ -20,22 +20,36 @@ namespace OnatOctopusTest.Extensions
             return htmlString;
         }
         public static string GetTagClass(int category, int articles)
-        { 
+        {
             var result = (category * 100) / articles;
 
+            //if (result <= 1)
+            //    return "tag1";
+            //if (result <= 4)
+            //    return "tag2";
+            //if (result <= 8)
+            //    return "tag3";
+            //if (result <= 12)
+            //    return "tag4";
+            //if (result <= 18)
+            //    return "tag5";
+            //if (result <= 30)
+            //    return "tag6";
+            //return result <= 50 ? "tag7" : "";
+
             if (result <= 1)
-                return "tag1";
+                return "0.5em";
             if (result <= 4)
-                return "tag2";
+                return "1em";
             if (result <= 8)
-                return "tag3";
+                return "2em";
             if (result <= 12)
-                return "tag4";
+                return "3em";
             if (result <= 18)
-                return "tag5";
+                return "4em";
             if (result <= 30)
-                return "tag6";
-            return result <= 50 ? "tag7" : "";
+                return "5em";
+            return result <= 50 ? "6em" : "";
         }
     }
 }
