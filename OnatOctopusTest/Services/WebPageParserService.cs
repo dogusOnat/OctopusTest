933a9f2ae7cb53dc951b36dcb22f23a20f9b1e33
dogusOnat <dogus.onat.erses@gmail.com>
2017-10-02 19:48:26 +0100

    base64 Decode error fixed stop words added

--- OnatOctopusTest/Services/WebPageParserService.cs
+++ OnatOctopusTest/Services/WebPageParserService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq; 
 using OnatOctopusTest.Extensions;
 using OnatOctopusTest.Core;
+using System.Text.RegularExpressions;
 
 namespace OnatOctopusTest.Services
 {
@@ -11,6 +12,8 @@ namespace OnatOctopusTest.Services
     {
         public Dictionary<string, int> GetWordFrequency(string websiteURL)
         {
+            Regex r = new Regex("^[a-zA-Z0-9]*$");
+
             HtmlWeb web = new HtmlWeb();
             if (!(websiteURL.StartsWith("http://", StringComparison.OrdinalIgnoreCase) || websiteURL.StartsWith("https://", StringComparison.OrdinalIgnoreCase)))
                 websiteURL = "http://" + websiteURL;
@@ -20,7 +23,7 @@ namespace OnatOctopusTest.Services
             string html = Worker.ClearHtml(doc.DocumentNode.OuterHtml);
 
             return html.Split(' ')
-                       .Where(x => x != string.Empty)
+                      .Where(x => x != string.Empty && !Worker.filters.Contains(x.ToLower()) && r.IsMatch(x))
                        .GroupBy(x => x)
                        .ToDictionary(x => x.Key, x => x.Count()); 
         }
